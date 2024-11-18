5a3dd5c977686a3c241bac24a96985ebf307c45c
Lalitha9300 <lalitha9300@gmail.com>
2024-11-18 20:40:10 +0530

    modified header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,11 +13,7 @@ const Header = () => {
           <li><a href=".home-container">Home</a></li>
           <li><a href=".about-container">About</a></li>
           <li><a href=".projects-containe">Projects</a></li>
-          <li><a href=".ontact-container">Contact</a></li>
-          {/* <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/contact">Contact</Link></li> */}
+          <li><a href=".">Contact</a></li>
         </ul>
         
       </nav>
